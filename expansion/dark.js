dd44cee57590adb67b703d216704085b18364a39
514K <43733430+514K@users.noreply.github.com>
2020-05-25 09:45:10 +0300

    v1.1

---
+++ expansion/dark.js
@@ -0,0 +1,154 @@
+"use strict"
+document.body.style.backgroundColor = "#1f1f1f";
+let elem;
+let elem1;
+
+try {
+    elem = document.getElementById("hdtbSum");
+    elem.style.background = "#1f1f1f";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem = document.getElementById("search");
+    elem.style.color = "white";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = elem.getElementsByClassName("iUh30");
+    for (let item of elem1){
+        item.style.color = "#5f6368";
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem = document.getElementById("fbar");
+    elem.style.background = "#1f1f1f";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = elem.getElementsByClassName("Q8LRLc")[0];
+    elem1.style.color = "#5f6368";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem = document.getElementById("Wprf1b");
+    elem.style.color = "#5f6368";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("sfbg nojsv")[0];
+    elem1.style.background = "#1f1f1f";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("WGYX8");
+    for (let item of elem1){
+        item.style.background = "#272727";
+        item.style.color = "#b7b7b7";
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.getElementById("irl_r_a");
+    elem1.style.color = "bdbdbd";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("ZTH1s cv2VAd");
+    for (const item of elem1) {
+        item.style.background = "#1f1f1f";
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("JRhSae");
+    for (const item of elem1) {
+        item.style.fontWeight = 600;
+        item.style.color = "#5157ff";
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("DKV0Md");
+    for (const item of elem1) {
+        item.style.fontWeight = 600;
+        item.style.color = "#5157ff";
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("RNNXgb")[0];
+    elem1.style.background = "#1f1f1f";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("gLFyf")[0];
+    elem1.style.color = "white";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("sbl1");
+    for (const item of elem1) {
+        item.style.color = "#a9a9a9";
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("aajZCb")[0];
+    elem1.style.background = "#353535";
+} catch (error) {
+    console.log(error);
+}
+
+
+try {
+    elem1 = document.body.getElementsByClassName("garHBe")[0];
+    elem1.style.color = "#b7b7b7";
+} catch (error) {
+    console.log(error);
+}
